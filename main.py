3958795d2ac8c61d9d8ead21e7985f5b8f7225ba
Kandhy-0X9 <kashengkandhy@gmail.com>
2026-03-02 13:10:25 -0500

    idk

--- main.py
+++ main.py
@@ -12,17 +12,13 @@ posY = window_height() // 2
 target_x = random.randint(-posX, posX)
 target_y = random.randint(-posY, posY)
 target_range = 30   # How close they need to be
-
 print(target_x, target_y)# for me to check the treasure
-
 # Function to Check Hit (returns a boolean)
 def hit_target(x, y):
     return abs(x - target_x) < target_range and abs(y - target_y) < target_range
-
 # Intro
 print("welcome to Treasure Hunt\nGood Luck...\n")
 repeat = True
-
 while repeat == True:
     # Ask user
     x = int(input("What are the coordinates for the treasure on the x-axis\nBetween -225 and 225: "))
@@ -30,7 +26,7 @@ while repeat == True:
     print("\nYou dug here",x , y)
     # check for the treasure
     if hit_target(x, y):
-        print("\nYou found the treasure\n")
+        print("You found the treasure\nTreasure was at:", target_x, target_y)
         penup()
         goto(x, y)
         begin_fill()
@@ -41,14 +37,12 @@ while repeat == True:
         repeat = False
         if repeat == False:
             break
-
     else:
         print("\nYou didn't find the treasure\n")
         color("red")
         penup()
         goto(x, y)
         pendown()
-        circle(12)
-        
+        circle(12)       
 # keep the window open
 done()
