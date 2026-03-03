3ff89a874ddd93c453b6cdd88b20ef0d4e69d6ca
Kandhy-0X9 <kashengkandhy@gmail.com>
2026-03-03 13:41:46 -0500

    jkljkhrb

--- main.py
+++ main.py
@@ -1,57 +1,87 @@
 from turtle import *
 import random
+import time
+import os
 # set up
 bgcolor("lightgreen")
 setup(650, 550)
 pensize(2.5)
 speed(0)
-hideturtle()
 # map
 bgpic(r"TreasureMap.gif")
-# write top
-penup()
-color("brown")
-goto(0, 230)
-write("Tracy's Treasure Hunt!",align="center",font=("Arial", 26, "bold"))
-# write bottom
-penup()
-color("black")
-goto(0, -250)
-write("Can you find her gold doubloon??",align="center",font=("Arial", 12, "italic"))
-# Random target area
+# random position for treasure
 target_x = random.randint(-220, 220)
 target_y = random.randint(-170, 170)
 target_range = 30   # How close they need to be
-# Function to Check Hit (returns a boolean)
+def treasureMark():
+    # stamp
+    penup()
+    shape("turtle")
+    goto(-175,235)
+    right(60)
+    color("red")
+    stamp()
+def treasure_text():
+    # write top
+    penup()
+    goto(0, 230)
+    write("Tracy's Treasure Hunt!",align="center",font=("Arial", 26, "bold"))
+    # write bottom
+    penup()
+    color("black")
+    goto(0, -250)
+    write("Can you find her gold doubloon??",align="center",font=("Arial", 12, "italic"))
 def hit_target(x, y):
     return abs(x - target_x) < target_range and abs(y - target_y) < target_range
+
 # Intro
 print("welcome to Treasure Hunt\nGood Luck...\n")
+treasureMark()
+treasure_text()
 repeat = True
 while repeat == True:
+    print(target_x, target_y)
     # Ask user
-    x = int(input("What are the coordinates for the treasure on the x-axis?\nMost likely between -220 and 220: "))
-    y = int(input("What are the coordinates for the treasure on the y-axis?\nMOst likely between -170 and 170: "))
+    x = int(textinput("What are the coordinates for the treasure on the x-axis?", "Most likely between -220 and 220: "))
+    y = int(textinput("What are the coordinates for the treasure on the y-axis?", "M0st likely between -170 and 170: "))
     print("\nYou dug here",x ,",", y)
+    # Move Turtle to Guess
+
     # check for the treasure
     if hit_target(x, y):
-        print("You found the treasure\nTreasure was at:", target_x,",",target_y)
+        hideturtle
+        print("You found the doubloon!")
+        color("black")
+        # Move Turtle to Guess
         penup()
         goto(x, y)
-        begin_fill()
         pendown()
-        circle(12)
-        color("yellow")
+        begin_fill()
+        circle(15)  
+        color("gold")
         end_fill()
+        penup()
+        forward(15)
+        color("black")
+        write(":D", font=("Monospace", 12, "italic"))
         repeat = False
         if repeat == False:
             break
+
     else:
-        print("\nYou didn't find the treasure\n")
+        hideturtle()
+        print("Miss! Try again :(")
         color("red")
+        # Move Turtle to Guess
         penup()
         goto(x, y)
         pendown()
-        circle(12)       
+        circle(15) 
+        penup()
+        forward(15)
+        color("black")
+        write(":(", font=("Monospace", 12, "italic"))    
+        time.sleep(2)# Allows you to see your coodinates
+hideturtle()
 # keep the window open
 done()
